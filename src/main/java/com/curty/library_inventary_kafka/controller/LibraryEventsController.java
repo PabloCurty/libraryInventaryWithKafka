fd354e5c79c938fa17ab4942656ad4a7083c3014
pablo.curty <pablo.curty@visagio.com>
2021-01-24 15:43:26 -0300

    configuration, create a new topic in local profile, send a message to 3 broker cluster with API, and error handle

--- src/main/java/com/curty/library_inventary_kafka/controller/LibraryEventsController.java
+++ src/main/java/com/curty/library_inventary_kafka/controller/LibraryEventsController.java
@@ -1,6 +1,9 @@
 package com.curty.library_inventary_kafka.controller;
 
 import com.curty.library_inventary_kafka.domain.LibraryEvent;
+import com.curty.library_inventary_kafka.producer.LibraryEventProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LibraryEventsController {
 
-    @PostMapping("/v1/libraryEvent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
+
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+
         //invoke kafka producer
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 
