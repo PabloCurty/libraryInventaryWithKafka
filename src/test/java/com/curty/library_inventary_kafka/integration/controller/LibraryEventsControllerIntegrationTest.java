5a67b276747e845aa83e0ae25527542345b9f432
pablo.curty <pablo.curty@visagio.com>
2021-01-26 12:05:33 -0300

    Integration test for POST endpoint /v1/libraryevent

---
+++ src/test/java/com/curty/library_inventary_kafka/integration/controller/LibraryEventsControllerIntegrationTest.java
@@ -0,0 +1,47 @@
+package com.curty.library_inventary_kafka.integration.controller;
+
+import com.curty.library_inventary_kafka.domain.Book;
+import com.curty.library_inventary_kafka.domain.LibraryEvent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//If you not provide the port, all the time we will launch the application in default port 88
+//It's can make a conflict, happened with me
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LibraryEventsControllerIntegrationTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void postLibraryEventTest() {
+
+        //scenario
+        Book book = Book.builder()
+                .bookId(123)
+                .bookAuthor("Pablo")
+                .bookName("Kafka Spring Boot")
+                .build();
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+        //We need to pass in restTemplate.exchange a LibraryEvent in a HttpEntity and HttpHeaders way
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("contet-type", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, headers);
+
+        //execution
+        ResponseEntity<LibraryEvent> responseEntity = restTemplate
+                .exchange("/v1/libraryevent", HttpMethod.POST, request, LibraryEvent.class);
+
+        //verification
+        //imported assertEquals JUnit at Static variable
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    }
+}
