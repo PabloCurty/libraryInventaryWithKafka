8e0781ca501443ba6fbbb5859995b02761f5fc15
pablo.curty <pablo.curty@visagio.com>
2021-01-26 13:20:09 -0300

    Integrate Kafka consumer in JUnit and consume the record

--- src/test/java/com/curty/library_inventary_kafka/integration/controller/LibraryEventsControllerIntegrationTest.java
+++ src/test/java/com/curty/library_inventary_kafka/integration/controller/LibraryEventsControllerIntegrationTest.java
@@ -2,15 +2,27 @@ package com.curty.library_inventary_kafka.integration.controller;
 
 import com.curty.library_inventary_kafka.domain.Book;
 import com.curty.library_inventary_kafka.domain.LibraryEvent;
+import com.curty.library_inventary_kafka.domain.LibraryEventType;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.HashMap;
+import java.util.Map;
 
 //If you not provide the port, all the time we will launch the application in default port 88
 //It's can make a conflict, happened with me
@@ -26,8 +38,27 @@ public class LibraryEventsControllerIntegrationTest {
     @Autowired
     TestRestTemplate restTemplate;
 
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    private Consumer<Integer, String> consumer;
+
+    @BeforeEach
+    void setUp() {
+        Map<String, Object> configs =
+                new HashMap<>(KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker ));
+        consumer = new DefaultKafkaConsumerFactory<>(
+                configs, new IntegerDeserializer(), new StringDeserializer()).createConsumer();
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+        consumer.close();
+    }
+
     @Test
-    void postLibraryEventTest() {
+    void postLibraryEventTest() throws InterruptedException {
 
         //scenario
         Book book = Book.builder()
@@ -44,12 +75,18 @@ public class LibraryEventsControllerIntegrationTest {
         headers.set("content-type", MediaType.APPLICATION_JSON.toString());
         HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, headers);
 
+        String expectedValue = "{\"libraryEventId\":null,\"libraryEventType\":\"NEW\",\"book\":{\"bookId\":123,\"bookName\":\"Kafka Spring Boot\",\"bookAuthor\":\"Pablo\"}}";
+
         //execution
         ResponseEntity<LibraryEvent> responseEntity = restTemplate
                 .exchange("/v1/libraryevent", HttpMethod.POST, request, LibraryEvent.class);
+        ConsumerRecord<Integer, String> consumerRecord = KafkaTestUtils.getSingleRecord(consumer, "library-events");
+        Thread.sleep(3000);
+        String value = consumerRecord.value();
 
         //verification
-        //imported assertEquals JUnit at Static variable
-        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        //imported assertEquals JUnit at Static variable is a problem to understand the code later
+        Assertions.assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        Assertions.assertEquals(expectedValue, value);
     }
 }
