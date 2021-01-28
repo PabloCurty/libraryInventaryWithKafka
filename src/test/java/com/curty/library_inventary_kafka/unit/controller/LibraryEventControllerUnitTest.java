7786047eef4104272227cf80925cf3c2f46e5264
pablo.curty <pablo.curty@visagio.com>
2021-01-28 12:35:55 -0300

    custom ErrorMessages using ControllerAdvice

--- src/test/java/com/curty/library_inventary_kafka/unit/controller/LibraryEventControllerUnitTest.java
+++ src/test/java/com/curty/library_inventary_kafka/unit/controller/LibraryEventControllerUnitTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LibraryEventsController.class)
@@ -45,16 +46,68 @@ public class LibraryEventControllerUnitTest {
                 .build();
 
         String json = objectMapper.writeValueAsString(libraryEvent);
+
+        //execution
         doNothing().when(libraryEventProducer)
                 .sendLibraryEvent_Approach2(isA(LibraryEvent.class));
 
-        //execution
+        //verification
         mockMvc.perform(post("/v1/libraryevent")
-        .content(json)
-        .contentType(MediaType.APPLICATION_JSON))
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+    }
 
+    // when book null the controller advice answer 'errorMessage : book - must not be null'"
+    @Test
+    void postLibraryEventUnitTest_BookNull_4xx() throws Exception {
+        //scenario
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(null)
+                .build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+        String expectedError = "book - must not be null";
+        //execution
+        doNothing().when(libraryEventProducer)
+                .sendLibraryEvent_Approach2(isA(LibraryEvent.class));
 
         //verification
+        mockMvc.perform(post("/v1/libraryevent")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+        .andExpect(content().string(expectedError));
+    }
+
+    // when some book field is null or Blank the controller advice answer 'errorMessage : book - must not be null'"
+    @Test
+    void postLibraryEventUnitTest_BookFieldNullOrBlank_4xx() throws Exception {
+        //scenario
+        Book book = Book.builder()
+                .bookId(null)
+                .bookAuthor(null)
+                .bookName("Kafka Spring Boot")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+        String expectedError = "book.bookAuthor - must not be blank, book.bookId - must not be null";
+
+        //execution
+        doNothing().when(libraryEventProducer)
+                .sendLibraryEvent_Approach2(isA(LibraryEvent.class));
+
+        //verification
+        mockMvc.perform(post("/v1/libraryevent")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+        .andExpect(content().string(expectedError));
     }
 }
